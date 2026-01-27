2b64fe9e57b3b68e95b85b6318b907dbd40d3c48
Kokc80 <kokc@inbox.ru>
2026-01-27 19:39:54 +0900

    получены вакансии

---
+++ src/cl_parser.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+
+import requests
+
+
+class Parser(ABC):
+    """
+    Класс Parser является абстрактным родительским классом
+    """
+    @abstractmethod
+    def load_vacancies(self, keyword) -> None:
+        pass
+
+    @abstractmethod
+    def _connect_to_api(self):
+        pass
+
+
+class HeadHunterAPI(Parser):
+    """Класс для получения вакансий с API HeadHunter"""
+    def __init__(self):
+        self.__url = 'https://api.hh.ru/vacancies'
+        self._headers = {'User-Agent': 'HH-User-Agent'}
+        self._params = {'text': '', 'page': 0, 'per_page': 100}
+        self._vacancies = []
+
+    def _connect_to_api(self):
+        """Метод подключения к API"""
+        # print ("connect")
+        response = requests.get(self.__url, headers=self._headers, params=self._params)
+        status = response.status_code
+        if status == 200:
+            print("connect 200")
+            return response
+        else:
+            return 'Ошибка при обращении к API - error'
+
+    def load_vacancies(self, keyword):
+        """Получение списка вакансий"""
+        self._params['text'] = keyword
+        while self._params.get('page') != 2:
+            # print("PAGE", self.params.get('page'))
+            response = requests.get(self.__url, headers=self._headers, params=self._params)
+            vacancies_items = response.json()['items']
+            # print("VAC\n", vacancies_items)
+            self._vacancies.extend(vacancies_items)
+            self._params['page'] += 1
+            return vacancies_items
+
+
+if __name__ == "__main__":
+    hh_api = HeadHunterAPI()
+    hh_api._connect_to_api()
+    api_vacantions = hh_api.load_vacancies("Python")
+    print("REZ VACANTIONS", api_vacantions)
--- src/functions.py
+++ src/functions.py
@@ -1,47 +1,36 @@
 import psycopg2
 
 
-def db_connect():
-    #psql - h < хост > -p < порт > -d < имя_базы_данных > -U < имя_пользователя >
-    conn = psycopg2.connect(
-        host="localhost",
-        # database="vacancy",
-        user="postgres",
-        password="678330",
-        port="5432"
-    )
+def db_connect(db_name: str):
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            # database="vacancy",
+            user="postgres",
+            password="678330",
+            port="5432"
+        )
+        conn.autocommit = True
+    except:
+        print("Ошибка подключения")
+    finally:
+        print("Соединение успешно")
+
+    db_name="vacancy_db"
     cur = conn.cursor()
 
     # Удаление BD
-    cur.execute("DROP DATABASE vacancydb;")
-    conn.commit()
-
+    try:
+        cur.execute(f"DROP DATABASE {db_name};")
+        print(f"Удаление БД '{db_name}' выполнено")
+    except:
+        print(f"Удаление БД не требуется, БД -'{db_name}' не существует")
+    finally:
+        print("OK")
     # Создание новой BD
-    cur.execute("CREATE DATABASE vacancydb;")
-    conn.commit()
+    cur.execute(f"CREATE DATABASE {db_name};")
+    print(f"Создание БД - {db_name}")
 
     # Закрытие курсора и соединения
     cur.close()
     conn.close()
-
-    # # Подключение к базе данных postgres
-    # conn = psycopg2.connect(
-    #     dbname="postgres",
-    #     user="postgres",
-    #     password="password",
-    #     host="localhost",
-    #     port="5432"
-    # )
-    #
-    # cur = conn.cursor()
-    #
-    # # Создание новой базы данных
-    # cur.execute("CREATE DATABASE selecteldb;)
-    # conn.commit()
-    #
-    # # Закрытие курсора и соединения
-    # cur.close()
-    # conn.close()
-
-print("hjgjhj")
-db_connect()
